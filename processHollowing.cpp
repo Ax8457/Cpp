62ebe08c0283cde13af4e3650a5f6ade02a4a226
Ax8457 <147101606+Ax8457@users.noreply.github.com>
2024-10-13 15:29:47 +0200

    Update processHollowing.cpp

--- processHollowing.cpp
+++ processHollowing.cpp
@@ -22,15 +22,6 @@ typedef NTSTATUS(WINAPI* _NtUnmapViewOfSection)(
     PVOID BaseAddress
     );
 
-typedef struct BASE_RELOCATION_BLOCK {
-    DWORD PageAddress;
-    DWORD BlockSize;
-} BASE_RELOCATION_BLOCK, * PBASE_RELOCATION_BLOCK;
-
-typedef struct BASE_RELOCATION_ENTRY {
-    USHORT Offset : 12;
-    USHORT Type : 4;
-} BASE_RELOCATION_ENTRY, * PBASE_RELOCATION_ENTRY;
 
 int main()
 {
